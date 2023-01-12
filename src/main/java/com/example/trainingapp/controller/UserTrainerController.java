6db872db7234700ff53b53d205d3875ff4745f14
Zigix <olekwszk7@gmail.com>
2023-01-12 18:46:14 +0100

    Add diet and training forms functionality

--- src/main/java/com/example/trainingapp/controller/UserTrainerController.java
+++ src/main/java/com/example/trainingapp/controller/UserTrainerController.java
@@ -1,5 +1,7 @@
 package com.example.trainingapp.controller;
 
+import com.example.trainingapp.domain.dto.DietDto;
+import com.example.trainingapp.domain.dto.TrainingDto;
 import com.example.trainingapp.domain.dto.UserTrainerDto;
 import com.example.trainingapp.domain.mapper.EntityDtoMapper;
 import com.example.trainingapp.domain.model.User;
@@ -10,8 +12,7 @@ import com.example.trainingapp.service.UserTrainerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
 public class UserTrainerController {
     private final UserContextService userContextService;
     private final UserTrainerRepository userTrainerRepository;
+    private final UserTrainerService userTrainerService;
     private final EntityDtoMapper mapper;
 
     @GetMapping("/user-trainer") // TODO: zobacz wszystkie polaczenia usera z trenerami
@@ -40,11 +42,39 @@ public class UserTrainerController {
     }
 
     @GetMapping("/user-trainer/{userTrainerId}")
-    public String showOneUserTrainerConnection(@RequestParam Long userTrainerId, Model model) {
+    public String showOneUserTrainerConnection(@PathVariable Long userTrainerId, Model model) {
         UserTrainer userTrainer = userTrainerRepository.getById(userTrainerId);
         User loggedUser = userContextService.getLoggedUser();
         model.addAttribute("userTrainer", mapper.toUserTrainerDto(userTrainer));
         model.addAttribute("loggedUser", loggedUser);
         return "";
+    }
+
+    @GetMapping("/user-trainer/{userTrainerId}/diet")
+    public String addDietForm(@PathVariable Long userTrainerId, Model model) {
+        DietDto dietDto = new DietDto();
+        dietDto.setUserTrainerId(userTrainerId);
+        model.addAttribute("diet", dietDto);
+        return ""; // TODO: <-- widok dla dodawania diety
+    }
+
+    @PostMapping("/user-trainer/{userTrainerId}/diet")
+    public String processDietForm(@PathVariable Long userTrainerId, @ModelAttribute DietDto dietDto) {
+        userTrainerService.addDiet(dietDto);
+        return ""; // TODO: <-- redirect na user training view
+    }
+
+    @GetMapping("/user-trainer/{userTrainerId}/training")
+    public String addTrainingForm(@PathVariable Long userTrainerId, Model model) {
+        TrainingDto trainingDto = new TrainingDto();
+        trainingDto.setUserTrainerId(userTrainerId);
+        model.addAttribute("training", trainingDto);
+        return ""; // TODO: <-- widok dla dodawania treningu
+    }
+
+    @PostMapping("/user-trainer/{userTrainerId}/training")
+    public String processTrainingForm(@PathVariable Long userTrainerId, @ModelAttribute TrainingDto trainingDto) {
+        userTrainerService.addTraining(trainingDto);
+        return ""; // TODO: <-- redirect na user training view
     }
 }
--- src/main/java/com/example/trainingapp/service/UserTrainerService.java
+++ src/main/java/com/example/trainingapp/service/UserTrainerService.java
@@ -1,10 +1,13 @@
 package com.example.trainingapp.service;
 
+import com.example.trainingapp.domain.dto.DietDto;
+import com.example.trainingapp.domain.dto.TrainingDto;
+import com.example.trainingapp.domain.mapper.EntityDtoMapper;
+import com.example.trainingapp.domain.model.Diet;
+import com.example.trainingapp.domain.model.Training;
 import com.example.trainingapp.domain.model.User;
 import com.example.trainingapp.domain.model.UserTrainer;
-import com.example.trainingapp.repository.TrainerDetailsRepository;
-import com.example.trainingapp.repository.UserRepository;
-import com.example.trainingapp.repository.UserTrainerRepository;
+import com.example.trainingapp.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,9 @@ import java.util.List;
 public class UserTrainerService {
     private final UserTrainerRepository userTrainerRepository;
     private final UserRepository userRepository;
+    private final DietRepository dietRepository;
+    private final TrainingRepository trainingRepository;
+    private final EntityDtoMapper mapper;
 
     public UserTrainer connectUserWithTrainer(Long trainerId, User loggedUser) {
         UserTrainer userTrainer = UserTrainer.builder()
@@ -22,5 +28,15 @@ public class UserTrainerService {
                 .trainer(userRepository.getById(trainerId))
                 .build();
         return userTrainerRepository.save(userTrainer);
+    }
+
+    public void addDiet(DietDto dietDto) {
+        Diet diet = mapper.toDiet(dietDto);
+        dietRepository.save(diet);
+    }
+
+    public void addTraining(TrainingDto trainingDto) {
+        Training training = mapper.toTraining(trainingDto);
+        trainingRepository.save(training);
     }
 }
